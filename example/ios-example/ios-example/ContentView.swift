81eb532046b018601ab8a5ecb1eede1d93442e5f
Hao Zhang <haozhang@CNMAC0365.local>
2021-12-15 20:50:33 +0800

    * added a swift / ios example

---
+++ example/ios-example/ios-example/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  ios-example
+//
+//  Created by Hao Zhang on 2021/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        var wallet = try? HdWallet.generateWallet(password: "")
+        var mnemonic = try? wallet?.getBackupMnemonicPhrase()
+        Text("mnemonic: " + mnemonic!)
+        
+        var address = try? wallet?.getDefaultAddress(coin: WalletCoin.cosmosSdk(network: Network.cosmosHub))
+        Text("address: " + address!)
+        var priv = try? wallet?.getKey(derivationPath: "m/44'/118'/0'/0/0")
+
+        var txinfo = CosmosSdkTxInfo.init(accountNumber: 1, sequenceNumber: 1, gasLimit: 100, feeAmount: SingleCoin.atom(amount: 100), timeoutHeight: 9000, memoNote: "memo", network: Network.cosmosHub)
+        
+        var siged_tx = try? buildSignedSingleMsgTx(txInfo: txinfo, msg: CosmosSdkMsg.bankSend(recipientAddress: "cosmos1rw2cc4hj6ahfk87pqh9znjzgmqwq8ec8nzt0e9", amount: SingleCoin.atom(amount: 20)), secretKey: priv!)
+        
+        
+        let hexArray = siged_tx?.compactMap { String(format: "%02x", $0).lowercased()}
+        let hexString = hexArray?.joined(separator: "")
+        Text("siged_tx: " + hexString!)
+
+        Text("end")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
