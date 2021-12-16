89b3abaf4da7cd8b58a9db754523120cb8efe3f3
SHH-HaoZhang <hao.zhang@crypto.com>
2021-12-16 15:31:22 +0800

    added a swift / ios example

---
+++ example/ios-example/ios-example/ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_exampleApp.swift
+//  ios-example
+//
+//  Created by Hao Zhang on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
