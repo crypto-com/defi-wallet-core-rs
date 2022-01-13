0690419b7a09dcf3d39b270295a2254e0daa0656
SHH-HaoZhang <hao.zhang@crypto.com>
2022-01-13 14:54:22 +0800

    update android build, separate module building

--- android_build.sh
+++ android_build.sh
@@ -94,22 +94,22 @@ else
         echo "x86_64 ndk installed."
 fi
 
-PATH=$PATH:`pwd`/NDK/arm64/bin cargo build --target aarch64-linux-android --release || exit 1
-PATH=$PATH:`pwd`/NDK/arm/bin cargo build --target armv7-linux-androideabi --release || exit 1
-PATH=$PATH:`pwd`/NDK/x86_64/bin cargo build --target x86_64-linux-android --release || exit 1
+PATH=$PATH:`pwd`/NDK/arm64/bin cargo build --target aarch64-linux-android -p defi-wallet-core-common --release || exit 1
+PATH=$PATH:`pwd`/NDK/arm/bin cargo build --target armv7-linux-androideabi -p defi-wallet-core-common --release || exit 1
+PATH=$PATH:`pwd`/NDK/x86_64/bin cargo build --target x86_64-linux-android -p defi-wallet-core-common --release || exit 1
 
 mkdir -p mobile_modules/android_module/dwclib/libs
 cp NDK/libs/jna.aar mobile_modules/android_module/dwclib/libs/
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a || exit 1
-cp target/aarch64-linux-android/release/libdefi_wallet_core_wasm.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so || exit 1
+cp target/aarch64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so || exit 1
 cp NDK/openssl/openssl-aarch64/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/ || exit 1
 cp NDK/openssl/openssl-aarch64/openssl-$OPENSSLTAG/libcrypto.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/ || exit 1
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a || exit 1
-cp target/armv7-linux-androideabi/release/libdefi_wallet_core_wasm.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so || exit 1
+cp target/armv7-linux-androideabi/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so || exit 1
 cp NDK/openssl/openssl-arm/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/ || exit 1
 cp NDK/openssl/openssl-arm/openssl-$OPENSSLTAG/libcrypto.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/ || exit 1
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64 || exit 1
-cp target/x86_64-linux-android/release/libdefi_wallet_core_wasm.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so || exit 1
+cp target/x86_64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so || exit 1
 mkdir -p mobile_modules/android_module/dwclib/src/main/java/com/defi/wallet/core/common || exit 1
 cp bindings/android/com/defi/wallet/core/common/common.kt mobile_modules/android_module/dwclib/src/main/java/com/defi/wallet/core/common/ || exit 1
 cp NDK/openssl/openssl-x86_64/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/ || exit 1
