36bb37d2fe19f8bc969f49f7982196ad4efc8591
Steven Gu <steven.gu@crypto.com>
2022-01-18 20:27:50 +0800

    Add crate `bip39` to support wallet recovered from different word mnemonic.

--- common/src/wallet.rs
+++ common/src/wallet.rs
@@ -1,13 +1,15 @@
 use std::sync::Arc;
 
 use crate::Network;
+use bip39::{Language, Mnemonic};
 use cosmrs::bip32::secp256k1::ecdsa::SigningKey;
-use cosmrs::bip32::{DerivationPath, Error, Mnemonic, PrivateKey, Seed, XPrv};
+use cosmrs::bip32::{self, DerivationPath, PrivateKey, Seed, XPrv};
 use cosmrs::crypto::PublicKey;
 use ethers::utils::hex::ToHex;
 use ethers::utils::secret_key_to_address;
 use rand_core::OsRng;
 use secrecy::{ExposeSecret, SecretString, Zeroize};
+use std::str::FromStr;
 
 /// describes what coin type to use (for HD derivation or address generation)
 pub enum WalletCoin {
@@ -49,7 +51,7 @@ pub enum HdWrapError {
     #[error("The length should be 64-bytes")]
     InvalidLength,
     #[error("HD wallet error: {0}")]
-    HDError(Error),
+    HDError(anyhow::Error),
     #[error("AccountId error: {0}")]
     AccountId(eyre::Report),
 }
@@ -85,20 +87,20 @@ impl HDWallet {
     ) -> Result<Self, HdWrapError> {
         let phrase = SecretString::new(mnemonic_phrase);
         let pass = SecretString::new(password.unwrap_or_default());
-        Self::recover_english(phrase, pass).map_err(HdWrapError::HDError)
+        Self::recover_english(phrase, pass)
     }
 
     /// returns the backup mnemonic phrase (if any)
     pub fn get_backup_mnemonic_phrase(&self) -> Option<String> {
-        self.mnemonic
-            .as_ref()
-            .map(|phrase| phrase.phrase().to_owned())
+        self.mnemonic.as_ref().map(|m| m.to_string())
     }
 
     /// generates the HD wallet and returns the backup phrase
     fn generate_english(password: SecretString) -> Self {
-        let mnemonic = Mnemonic::random(&mut OsRng, Default::default());
-        let seed = mnemonic.to_seed(password.expose_secret());
+        let mut rng = rand::thread_rng();
+        let mnemonic = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
+        let seed = mnemonic.to_seed_normalized(password.expose_secret());
+        let seed = Seed::new(seed);
         Self {
             seed,
             mnemonic: Some(mnemonic),
@@ -109,10 +111,12 @@ impl HDWallet {
     fn recover_english(
         mnemonic_phrase: SecretString,
         password: SecretString,
-    ) -> Result<Self, Error> {
-        let mnemonic = Mnemonic::new(mnemonic_phrase.expose_secret(), Default::default())
-            .map_err(|_| Error::Decode)?;
-        let seed = mnemonic.to_seed(password.expose_secret());
+    ) -> Result<Self, HdWrapError> {
+        let mnemonic = Mnemonic::from_str(mnemonic_phrase.expose_secret())
+            .map_err(|e| HdWrapError::HDError(e.into()))?;
+        let seed = mnemonic.to_seed_normalized(password.expose_secret());
+        let seed = Seed::new(seed);
+
         Ok(Self {
             seed,
             mnemonic: Some(mnemonic),
@@ -127,10 +131,10 @@ impl HDWallet {
         };
         let derivation_path: DerivationPath = format!("m/44'/{}'/0'/0/{}", coin_type, index)
             .parse()
-            .map_err(HdWrapError::HDError)?;
+            .map_err(|e: bip32::Error| HdWrapError::HDError(e.into()))?;
 
-        let child_xprv =
-            XPrv::derive_from_path(&self.seed, &derivation_path).map_err(HdWrapError::HDError)?;
+        let child_xprv = XPrv::derive_from_path(&self.seed, &derivation_path)
+            .map_err(|e| HdWrapError::HDError(e.into()))?;
         coin.derive_address(child_xprv.private_key())
             .map_err(HdWrapError::AccountId)
     }
@@ -142,10 +146,11 @@ impl HDWallet {
 
     /// return the secret key for a given derivation path
     pub fn get_key(&self, derivation_path: String) -> Result<Arc<SecretKey>, HdWrapError> {
-        let derivation_path: DerivationPath =
-            derivation_path.parse().map_err(HdWrapError::HDError)?;
-        let child_xprv =
-            XPrv::derive_from_path(&self.seed, &derivation_path).map_err(HdWrapError::HDError)?;
+        let derivation_path: DerivationPath = derivation_path
+            .parse()
+            .map_err(|e: bip32::Error| HdWrapError::HDError(e.into()))?;
+        let child_xprv = XPrv::derive_from_path(&self.seed, &derivation_path)
+            .map_err(|e| HdWrapError::HDError(e.into()))?;
         Ok(Arc::new(SecretKey(child_xprv.private_key().clone())))
     }
 }
@@ -180,33 +185,34 @@ impl Default for SecretKey {
 
 #[cfg(test)]
 mod tests {
-
     use crate::*;
-    use secrecy::SecretString;
 
     #[test]
-    fn hd_wallet_works() {
+    fn test_wallet_recovered_from_24_word_mnemonic() {
         let words = "dune car envelope chuckle elbow slight proud fury remove candy uphold puzzle call select sibling sport gadget please want vault glance verb damage gown";
-        let phrase = SecretString::from(words.to_string());
-        let password = SecretString::from("".to_string());
 
-        let wallet = HDWallet::recover_english(phrase, password).expect("wallet");
+        let wallet = HDWallet::recover_wallet(words.to_owned(), Some("".to_owned()))
+            .expect("Failed to recover wallet");
+        assert_eq!(wallet.get_backup_mnemonic_phrase(), Some(words.to_owned()));
+
         let default_cosmos_address = wallet
             .get_default_address(WalletCoin::CosmosSDK {
                 network: Network::CryptoOrgMainnet,
             })
-            .expect("address");
+            .expect("Failed to get default Cosmos address");
         assert_eq!(
             default_cosmos_address,
             "cro1u9q8mfpzhyv2s43js7l5qseapx5kt3g2rf7ppf"
         );
+
         let default_eth_address = wallet
             .get_default_address(WalletCoin::Ethereum)
-            .expect("address");
+            .expect("Failed to get default Eth address");
         assert_eq!(
             default_eth_address,
             "0x2c600e0a72b3ae39e9b27d2e310b180abe779368"
         );
+
         let cosmos_address = wallet
             .get_address(
                 WalletCoin::CosmosSDK {
@@ -214,11 +220,12 @@ mod tests {
                 },
                 1,
             )
-            .expect("address");
+            .expect("Failed to get Cosmos address");
         assert_eq!(cosmos_address, "cro1g8w7w0kdx0hfv4eqhmv8avxnf7qruchg9pk3v2");
+
         let eth_address = wallet
             .get_address(WalletCoin::Ethereum, 1)
-            .expect("address");
+            .expect("Failed to get Eth address");
         assert_eq!(eth_address, "0x5a64bef6db23fc854e79eea9e630ccb9301629cb");
 
         let private_key = wallet
@@ -228,6 +235,59 @@ mod tests {
         let expected_key = [
             212, 154, 121, 125, 182, 59, 97, 193, 72, 209, 118, 126, 97, 111, 241, 92, 61, 217,
             200, 59, 99, 203, 166, 28, 33, 142, 161, 114, 242, 56, 98, 42,
+        ]
+        .to_vec();
+        assert_eq!(raw_key, expected_key);
+    }
+
+    #[test]
+    fn test_wallet_recovered_from_12_word_mnemonic() {
+        let words = "guard input oyster oyster slot doctor repair shed soon assist blame power";
+
+        let wallet = HDWallet::recover_wallet(words.to_owned(), Some("".to_owned()))
+            .expect("Failed to recover wallet");
+        assert_eq!(wallet.get_backup_mnemonic_phrase(), Some(words.to_owned()));
+
+        let default_cosmos_address = wallet
+            .get_default_address(WalletCoin::CosmosSDK {
+                network: Network::CryptoOrgMainnet,
+            })
+            .expect("Failed to get default Cosmos address");
+        assert_eq!(
+            default_cosmos_address,
+            "cro16edxe89pn8ly9c7cy702x9e62fdvf3k9tnzycj"
+        );
+
+        let default_eth_address = wallet
+            .get_default_address(WalletCoin::Ethereum)
+            .expect("Failed to get default Eth address");
+        assert_eq!(
+            default_eth_address,
+            "0xda25e7a4b1bda34e303e6d7f22abef78ce9a55db"
+        );
+
+        let cosmos_address = wallet
+            .get_address(
+                WalletCoin::CosmosSDK {
+                    network: Network::CryptoOrgMainnet,
+                },
+                1,
+            )
+            .expect("Failed to get Cosmos address");
+        assert_eq!(cosmos_address, "cro1keycl6d55fnlzwgfdufl53vuf95uvxnry6uj2q");
+
+        let eth_address = wallet
+            .get_address(WalletCoin::Ethereum, 1)
+            .expect("Failed to get Eth address");
+        assert_eq!(eth_address, "0x74aeb73c4f6c10750bcd8608b0347f3e4750151c");
+
+        let private_key = wallet
+            .get_key("m/44'/394'/0'/0/0".to_string())
+            .expect("key");
+        let raw_key = private_key.0.to_bytes().to_vec();
+        let expected_key = [
+            46, 156, 107, 197, 216, 223, 81, 119, 105, 126, 144, 232, 123, 208, 152, 210, 214, 22,
+            95, 9, 97, 149, 215, 143, 118, 204, 161, 206, 203, 243, 117, 37,
         ]
         .to_vec();
         assert_eq!(raw_key, expected_key);
