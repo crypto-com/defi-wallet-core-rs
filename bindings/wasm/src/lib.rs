50d27cd69feae946cf569ada43c468ece1419000
stevenatcrypto <88301195+stevenatcrypto@users.noreply.github.com>
2022-01-24 10:22:58 +0800

    Problem: second argument `word_count` of function `generate_wallet` is mandatory (fix #96) (#99)
    Solution: make the second argument optional

--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -92,8 +92,11 @@ impl From<MnemonicWordCount> for defi_wallet_core_common::MnemonicWordCount {
 impl Wallet {
     /// generate a random wallet (with an optional password)
     #[wasm_bindgen(constructor)]
-    pub fn new(password: Option<String>, word_count: MnemonicWordCount) -> Result<Wallet, JsValue> {
-        let wallet = HDWallet::generate_wallet(password, word_count.into())
+    pub fn new(
+        password: Option<String>,
+        word_count: Option<MnemonicWordCount>,
+    ) -> Result<Wallet, JsValue> {
+        let wallet = HDWallet::generate_wallet(password, word_count.map(|val| val.into()))
             .map_err(|e| JsValue::from_str(&format!("error: {}", e)))?;
         Ok(Self { wallet })
     }
