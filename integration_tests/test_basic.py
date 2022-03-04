c6435697abfe23e1a554a5cc7eea34abc0d9c34c
Damon Chen <damon.chen@crypto.com>
2022-03-04 17:51:00 +0800

    Run cppexmple in python

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
+from pathlib import Path
+
 from .utils import ADDRS, CONTRACTS, KEYS, Contract
 
 
@@ -27,15 +31,21 @@ def test_basic(chainmain, cronos):
 
     ERC721 = Contract(
         CONTRACTS["TestERC721"],
-        KEYS["validator"],
+        KEYS["signer1"],
     )
 
     contract = ERC721.deploy(w3, "MyTestERC721Token", "MyTestERC721")
-    tx = contract.functions.mint(ADDRS["community"], 1).buildTransaction(
-        {"from": ADDRS["validator"], "gas": 500000}
+    print("Signer1 address:", ADDRS["signer1"])
+    print("ERC721 address:", contract.address)
+    tx = contract.functions.mint(ADDRS["signer1"], 1).buildTransaction(
+        {"from": ADDRS["signer1"], "gas": 500000}
     )
     ERC721.send(tx)
     print("Name:", contract.functions.name().call())
     print("Symbol:", contract.functions.symbol().call())
-    print("Balance:", contract.functions.balanceOf(ADDRS["community"]).call())
-    assert contract.functions.balanceOf(ADDRS["community"]).call() == 1
+    print("Balance:", contract.functions.balanceOf(ADDRS["signer1"]).call())
+    assert contract.functions.balanceOf(ADDRS["signer1"]).call() == 1
+
+    cmd = Path(__file__).parent / "../example/cpp-example/cppexample"
+    output = subprocess.getoutput(str(cmd))
+    print(output)
