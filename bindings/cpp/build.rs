0b60d9423a5284af41cfda45da0b905777980849
Damon Chen <damon.chen@crypto.com>
2022-03-14 16:49:52 +0800

    Not moving main.cc and cronos.cc

--- bindings/cpp/build.rs
+++ bindings/cpp/build.rs
@@ -1,12 +1,12 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .flag_if_supported("-std=c++11")
-        .file("src/main.cc")
-        .file("src/cronos.cc")
+        .file("../../example/cpp-example/main.cc")
+        .file("../../example/cpp-example/cronos.cc")
         .compile("defi_wallet_core");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/nft.rs");
-    println!("cargo:rerun-if-changed=src/main.cc");
-    println!("cargo:rerun-if-changed=src/cronos.cc");
+    println!("cargo:rerun-if-changed=../../example/cpp-example/main.cc");
+    println!("cargo:rerun-if-changed=../../example/cpp-example/cronos.cc");
 }
