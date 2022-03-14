a518f087462b081fd9899f2d15cca7e4bb0f5ad1
Damon Chen <damon.chen@crypto.com>
2022-03-14 16:33:52 +0800

    Problem: Cxx example should be compiled in build.rs
    Close:
    
    Solution:
    - Move example/cpp-example/main.cc and example/cpp-example/cronos.cc to bindings/cpp/src
    - Compile main.cc and cronos.cc with cargo build
    - Copy the cxxbridge output files, main.cc and cronos.cc to cpp_example

--- bindings/cpp/build.rs
+++ bindings/cpp/build.rs
@@ -1,5 +1,12 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .flag_if_supported("-std=c++11")
+        .file("src/main.cc")
+        .file("src/cronos.cc")
         .compile("defi_wallet_core");
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src/nft.rs");
+    println!("cargo:rerun-if-changed=src/main.cc");
+    println!("cargo:rerun-if-changed=src/cronos.cc");
 }
