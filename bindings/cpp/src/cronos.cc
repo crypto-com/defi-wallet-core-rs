5388a80f2e235fe912ae857158173b3120a8027b
Damon Chen <damon.chen@crypto.com>
2022-03-14 16:37:55 +0800

    Add cronos.cc and main.cc

---
+++ bindings/cpp/src/cronos.cc
@@ -0,0 +1,87 @@
+#include "defi-wallet-core-cpp/src/lib.rs.h"
+#include "rust/cxx.h"
+#include <iostream>
+
+void cronos_process();
+using namespace std;
+using namespace org::defi_wallet_core;
+
+std::string getEnv(std::string key);
+
+rust::cxxbridge1::Box<Wallet>
+createWallet(rust::cxxbridge1::String mymnemonics);
+
+void cronos_process() {
+  std::cout << "cronos process" << std::endl;
+  rust::cxxbridge1::String mymnemonics = getEnv("MYMNEMONICS");
+  rust::cxxbridge1::String mycronosrpc = getEnv("MYCRONOSRPC");
+  rust::cxxbridge1::Box<Wallet> mywallet = createWallet(mymnemonics);
+  rust::cxxbridge1::String myaddress1 = mywallet->get_eth_address(0);
+  rust::cxxbridge1::String myaddress2 = mywallet->get_eth_address(1);
+  auto nonce1 = get_eth_nonce(myaddress1.c_str(), mycronosrpc);
+  char hdpath[100];
+  int cointype = 60;
+  int chainid = 777; // defined in cronos-devnet.yaml
+  snprintf(hdpath, sizeof(hdpath), "m/44'/%d'/0'/0/0", cointype);
+  rust::cxxbridge1::Box<PrivateKey> privatekey = mywallet->get_key(hdpath);
+  rust::cxxbridge1::Vec<uint8_t> data;
+  org::defi_wallet_core::EthTxInfoRaw eth_tx_info = new_eth_tx_info();
+  cout << myaddress2 << endl;
+  eth_tx_info.to_address = myaddress2.c_str();
+  eth_tx_info.nonce = nonce1;
+  eth_tx_info.amount = "1";
+  eth_tx_info.amount_unit = org::defi_wallet_core::EthAmount::EthDecimal;
+  rust::Vec<::std::uint8_t> signedtx =
+      build_eth_signed_tx(eth_tx_info, chainid, true, *privatekey);
+  rust::cxxbridge1::String balance =
+      get_eth_balance(myaddress1.c_str(), mycronosrpc);
+  cout << "address=" << myaddress1.c_str() << " balance=" << balance << endl;
+  rust::cxxbridge1::String txhash =
+      broadcast_eth_signed_raw_tx(signedtx, mycronosrpc);
+  cout << "txhash=" << txhash << endl;
+  balance = get_eth_balance(myaddress1.c_str(), mycronosrpc);
+  cout << "address=" << myaddress1.c_str() << " balance=" << balance << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc20_details =
+      erc20_balance("0x5003c1fcc043D2d81fF970266bf3fa6e8C5a1F3A");
+  rust::cxxbridge1::String erc20_balance =
+      get_contract_balance(myaddress1, *erc20_details, mycronosrpc);
+  cout << "GOLD balance=" << erc20_balance.c_str() << endl;
+
+
+  rust::cxxbridge1::Box<ContractBalance> erc721_details =
+      erc721_balance("0x2305f3980715c9D247455504080b41072De38aB9");
+  rust::cxxbridge1::String erc721_balance =
+      get_contract_balance(myaddress1, *erc721_details, mycronosrpc);
+  cout << "GameItem balance=" << erc721_balance.c_str() << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc1155_details_0 =
+      erc1155_balance("0x939D7350c54228e4958e05b65512C4a5BB6A2ACc", "0");
+  rust::cxxbridge1::String erc1155_balance_0 =
+      get_contract_balance(myaddress1, *erc1155_details_0, mycronosrpc);
+  cout << "Balance of GOLD=" << erc1155_balance_0.c_str() << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc1155_details_1 =
+      erc1155_balance("0x939D7350c54228e4958e05b65512C4a5BB6A2ACc", "1");
+  rust::cxxbridge1::String erc1155_balance_1 =
+      get_contract_balance(myaddress1, *erc1155_details_1, mycronosrpc);
+  cout << "Balance of SILVER=" << erc1155_balance_1.c_str() << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc1155_details_2 =
+      erc1155_balance("0x939D7350c54228e4958e05b65512C4a5BB6A2ACc", "2");
+  rust::cxxbridge1::String erc1155_balance_2 =
+      get_contract_balance(myaddress1, *erc1155_details_2, mycronosrpc);
+  cout << "Balance of THORS_HAMMER=" << erc1155_balance_2.c_str() << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc1155_details_3 =
+      erc1155_balance("0x939D7350c54228e4958e05b65512C4a5BB6A2ACc", "3");
+  rust::cxxbridge1::String erc1155_balance_3 =
+      get_contract_balance(myaddress1, *erc1155_details_3, mycronosrpc);
+  cout << "Balance of SWORD=" << erc1155_balance_3.c_str() << endl;
+
+  rust::cxxbridge1::Box<ContractBalance> erc1155_details_4 =
+      erc1155_balance("0x939D7350c54228e4958e05b65512C4a5BB6A2ACc", "4");
+  rust::cxxbridge1::String erc1155_balance_4 =
+      get_contract_balance(myaddress1, *erc1155_details_4, mycronosrpc);
+  cout << "Balance of SHIELD=" << erc1155_balance_4.c_str() << endl;
+}
---
+++ bindings/cpp/src/main.cc
@@ -0,0 +1,141 @@
+#include "defi-wallet-core-cpp/src/lib.rs.h"
+#include "rust/cxx.h"
+#include <cassert>
+#include <iostream>
+void cronos_process();
+using namespace std;
+using namespace org::defi_wallet_core;
+CosmosSDKTxInfoRaw build_txinfo() {
+  CosmosSDKTxInfoRaw ret;
+  ret.account_number = 0;
+  ret.sequence_number = 0;
+  ret.gas_limit = 100000;
+  ret.fee_amount = 1000000;
+  ret.fee_denom = "basecro";
+  ret.timeout_height = 0;
+  ret.memo_note = "";
+  ret.chain_id = "";
+  ret.coin_type = 394;
+  ret.bech32hrp = "cro";
+  return ret;
+}
+string getEnv(string key) {
+  string ret;
+  if (getenv(key.c_str()) != nullptr) {
+    ret = getenv(key.c_str());
+  }
+  return ret;
+}
+rust::cxxbridge1::Box<Wallet>
+createWallet(rust::cxxbridge1::String mymnemonics) {
+
+  try {
+    rust::cxxbridge1::Box<Wallet> mywallet = restore_wallet(mymnemonics, "");
+    return mywallet;
+  } catch (const rust::cxxbridge1::Error &e) {
+    cout << "invalid mnemonics" << endl;
+    cout << "export MYMNEMONICS=<your mnemonics>" << endl;
+    throw e;
+  }
+}
+
+void process() {
+  cout << "defi-wallet-core-rs cpp-example" << endl;
+  CosmosSDKTxInfoRaw tx_info = build_txinfo();
+
+  cout << "declare these environment variables:" << endl;
+  cout << "export MYMNEMONICS=\"your mnemonics\"" << endl;
+  cout << "export MYCOSMOSRPC=\"http://yourcosmosnode:1317\"" << endl;
+  cout << "export MYTENDERMINTRPC=\"http://yourcosmosnode:26657\"" << endl;
+  cout << "export MYGRPC=\"http://yourcosmosnode:9091\"" << endl;
+  cout << "export MYCHAINID=your-chainid-1" << endl;
+  cout << "export MYFROM=cro1yourwalletaddress" << endl;
+  cout << "export MYTO=cro1yourreceiveraddress" << endl;
+  cout << "------------------------------------------------------" << endl;
+
+  rust::cxxbridge1::String mymnemonics = getEnv("MYMNEMONICS");
+  string mychainid = getEnv("MYCHAINID");
+  string myfrom = getEnv("MYFROM");
+  string myto = getEnv("MYTO");
+  string myamount = getEnv("MYAMOUNT");
+  string myservercosmos = getEnv("MYCOSMOSRPC");         /* 1317 port */
+  string myservertendermint = getEnv("MYTENDERMINTRPC"); /* 26657 port */
+  string mygrpc = getEnv("MYGRPC");                      /* 9091 port */
+  rust::cxxbridge1::Box<Wallet> mywallet = createWallet(mymnemonics);
+  cout << "transfer from " << myfrom << " to " << myto << " amount " << myamount
+       << endl;
+  rust::cxxbridge1::String success, fail;
+  rust::cxxbridge1::String result =
+      mywallet->get_default_address(CoinType::CryptoOrgMainnet);
+  rust::cxxbridge1::String balance =
+      query_account_balance(myservercosmos, myfrom, tx_info.fee_denom, 1);
+  cout << "balance=" << balance.c_str() << endl;
+  rust::cxxbridge1::String detailjson =
+      query_account_details(myservercosmos, myfrom);
+  cout << "detailjson=" << detailjson.c_str() << endl;
+  CosmosAccountInfoRaw detailinfo =
+      query_account_details_info(myservercosmos, myfrom);
+  tx_info.account_number = detailinfo.account_number;
+  tx_info.sequence_number = detailinfo.sequence_number;
+  tx_info.chain_id = mychainid;
+  char hdpath[100];
+  snprintf(hdpath, sizeof(hdpath), "m/44'/%d'/0'/0/0", tx_info.coin_type);
+  rust::cxxbridge1::Box<PrivateKey> privatekey = mywallet->get_key(hdpath);
+  rust::cxxbridge1::Vec<uint8_t> signedtx =
+      get_single_bank_send_signed_tx(tx_info, *privatekey, myto, 1, "basecro");
+  broadcast_tx(myservertendermint, signedtx);
+
+  rust::cxxbridge1::Box<GrpcClient> grpc_client = new_grpc_client(mygrpc);
+  // grpc_client->supply("hello", "world");
+
+  rust::cxxbridge1::Vec<DenomRaw> denoms = grpc_client->denoms();
+  cout << denoms.size() << endl;
+}
+
+void test_login() {
+  cout << "testing login" << endl;
+
+  // no \n in end of string
+  std::string info =
+      "service.org wants you to sign in with your Ethereum account:\n"
+      "0xD09F7C8C4529CB5D387AA17E33D707C529A6F694\n"
+      "\n"
+      "I accept the ServiceOrg Terms of Service: https://service.org/tos\n"
+      "\n"
+      "URI: https://service.org/login\n"
+      "Version: 1\n"
+      "Chain ID: 1\n"
+      "Nonce: 32891756\n"
+      "Issued At: 2021-09-30T16:25:24Z\n"
+      "Resources:\n"
+      "- ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/\n"
+      "- https://example.com/my-web2-claim.json";
+  rust::cxxbridge1::Box<CppLoginInfo> logininfo = new_logininfo(info);
+
+  rust::cxxbridge1::String mymnemonics = getEnv("MYMNEMONICS");
+  rust::cxxbridge1::Box<Wallet> mywallet = createWallet(mymnemonics);
+
+  char hdpath[100];
+  int coin_type = 60; // eth cointype
+  snprintf(hdpath, sizeof(hdpath), "m/44'/%d'/0'/0/0", coin_type);
+  rust::cxxbridge1::Box<PrivateKey> privatekey = mywallet->get_key(hdpath);
+
+  rust::cxxbridge1::String default_address =
+      mywallet->get_default_address(CoinType::CronosMainnet);
+  rust::cxxbridge1::Vec<uint8_t> signature =
+      logininfo->sign_logininfo(*privatekey);
+  assert(signature.size() == 65);
+  rust::Slice<const uint8_t> slice{signature.data(), signature.size()};
+  logininfo->verify_logininfo(slice);
+}
+
+int main() {
+  try {
+    process();        // chain-main
+    test_login();     // decentralized login
+    cronos_process(); // cronos
+  } catch (const rust::cxxbridge1::Error &e) {
+    cout << "error:" << e.what() << endl;
+  }
+  return 0;
+}
