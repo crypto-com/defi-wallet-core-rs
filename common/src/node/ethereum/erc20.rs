c3cb7fbec05589ecdd39d844e30eb3e6a2fd530f
Damon Chen <damon.chen@crypto.com>
2022-03-31 14:58:26 +0800

    Problem: Missing contract approval for cpp binding
    Close: #256
    Solutions:
    - Add contrct approval bindings
    - Improve EthError (include url::parseError)

--- common/src/node/ethereum/erc20.rs
+++ common/src/node/ethereum/erc20.rs
@@ -1,25 +1,40 @@
+use super::address_from_str;
 use crate::contract::{Contract, ContractCall};
 use crate::EthError;
-use ethers::prelude::{Http, Provider};
+use ethers::prelude::{Http, Provider, U256};
 
 pub async fn get_name(contract_address: &str, web3api_url: &str) -> Result<String, EthError> {
-    let client = Provider::<Http>::try_from(web3api_url).map_err(|_| EthError::NodeUrl)?;
+    let client = Provider::<Http>::try_from(web3api_url).map_err(EthError::NodeUrl)?;
     let contract = Contract::new_erc20(contract_address, client)?;
     let call = contract.name();
     ContractCall::from(call).call().await
 }
 
 pub async fn get_symbol(contract_address: &str, web3api_url: &str) -> Result<String, EthError> {
-    let client = Provider::<Http>::try_from(web3api_url).map_err(|_| EthError::NodeUrl)?;
+    let client = Provider::<Http>::try_from(web3api_url).map_err(EthError::NodeUrl)?;
     let contract = Contract::new_erc20(contract_address, client)?;
     let call = contract.symbol();
     ContractCall::from(call).call().await
 }
 
 pub async fn get_decimals(contract_address: &str, web3api_url: &str) -> Result<u8, EthError> {
-    let client = Provider::<Http>::try_from(web3api_url).map_err(|_| EthError::NodeUrl)?;
+    let client = Provider::<Http>::try_from(web3api_url).map_err(EthError::NodeUrl)?;
     let contract = Contract::new_erc20(contract_address, client)?;
     let call = contract.decimals();
+    ContractCall::from(call).call().await
+}
+
+pub async fn get_allowance(
+    contract_address: &str,
+    owner: &str,
+    spender: &str,
+    web3api_url: &str,
+) -> Result<U256, EthError> {
+    let client = Provider::<Http>::try_from(web3api_url).map_err(EthError::NodeUrl)?;
+    let contract = Contract::new_erc20(contract_address, client)?;
+    let owner = address_from_str(owner)?;
+    let spender = address_from_str(spender)?;
+    let call = contract.allowance(owner, spender);
     ContractCall::from(call).call().await
 }
 
@@ -39,4 +54,15 @@ pub fn get_symbol_blocking(contract_address: &str, web3api_url: &str) -> Result<
 pub fn get_decimals_blocking(contract_address: &str, web3api_url: &str) -> Result<u8, EthError> {
     let rt = tokio::runtime::Runtime::new().map_err(|_err| EthError::AsyncRuntimeError)?;
     rt.block_on(get_decimals(contract_address, web3api_url))
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn get_allowance_blocking(
+    contract_address: &str,
+    owner: &str,
+    spender: &str,
+    web3api_url: &str,
+) -> Result<U256, EthError> {
+    let rt = tokio::runtime::Runtime::new().map_err(|_err| EthError::AsyncRuntimeError)?;
+    rt.block_on(get_allowance(contract_address, owner, spender, web3api_url))
 }
