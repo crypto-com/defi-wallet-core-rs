9b5868e59a8e682de6bed997f65521507bcd835a
SHH-HaoZhang <hao.zhang@crypto.com>
2022-04-29 05:59:00 +0800

    fix ci error

--- ios_build.sh
+++ ios_build.sh
@@ -19,8 +19,8 @@ cd mobile_modules/ios_module/dwclib/
 type strip || exit 1
 # Because the build needs to be signed, so comment first
 # xcodebuild SYMROOT="./build" -configuration Release -target dwclib -arch arm64 -sdk iphoneos build || exit 1
-# strip --strip-all build/Release-iphoneos/dwclib.framework/dwclib || exit 1
+# strip build/Release-iphoneos/dwclib.framework/dwclib || exit 1
 xcodebuild SYMROOT="./build" -configuration Release -target dwclib -arch x86_64 -sdk iphonesimulator build || exit 1
-strip --strip-all build/Release-iphoneos/dwclib.framework/dwclib || exit 1
+strip build/Release-iphoneos/dwclib.framework/dwclib || exit 1
 xcodebuild -scheme dwclib -destination 'platform=iOS Simulator,name=iPhone 12' test || exit 1
 cd -
