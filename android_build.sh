54035924fc3215edfa1d95c963d51dfa730c6bb8
SHH-HaoZhang <hao.zhang@crypto.com>
2022-04-29 13:49:00 +0800

    fix ci error

--- android_build.sh
+++ android_build.sh
@@ -104,17 +104,17 @@ mkdir -p mobile_modules/android_module/dwclib/libs
 cp NDK/libs/jna.aar mobile_modules/android_module/dwclib/libs/
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a || exit 1
 cp target/aarch64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so || exit 1
-strip --strip-all mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so
+strip mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so
 cp NDK/openssl/openssl-aarch64/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/ || exit 1
 cp NDK/openssl/openssl-aarch64/openssl-$OPENSSLTAG/libcrypto.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/ || exit 1
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a || exit 1
 cp target/armv7-linux-androideabi/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so || exit 1
-strip --strip-all mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so
+strip mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so
 cp NDK/openssl/openssl-arm/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/ || exit 1
 cp NDK/openssl/openssl-arm/openssl-$OPENSSLTAG/libcrypto.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/ || exit 1
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64 || exit 1
 cp target/x86_64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so || exit 1
-strip --strip-all mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so
+strip mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so
 mkdir -p mobile_modules/android_module/dwclib/src/main/java/com/defi/wallet/core/common || exit 1
 cp bindings/android/com/defi/wallet/core/common/common.kt mobile_modules/android_module/dwclib/src/main/java/com/defi/wallet/core/common/ || exit 1
 cp NDK/openssl/openssl-x86_64/openssl-$OPENSSLTAG/libssl.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/ || exit 1
