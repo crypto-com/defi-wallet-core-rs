2dcc12538ea62c2c5351a70f95ca79c2430a3afa
Steven Gu <steven.gu@crypto.com>
2022-05-06 10:00:39 +0800

    Implement parsing `AuthInfo` from Protobuf.

--- common/src/transaction/cosmos_sdk/parser.rs
+++ common/src/transaction/cosmos_sdk/parser.rs
@@ -1,10 +1,16 @@
-/// Cosmos parser is used to deserialize Protobuf or JSON to specified structs. The parsed
-/// instances could be encoded to a JSON string for display, and `CosmosSDKMsg`s could be used to
-/// build a new transaction.
-/// FIXME: It seems that these structs are only used for Cosmos parsing for now. They could be
-/// moved to `cosmos_sdk.rs` if reusable.
+// Cosmos parser is used to deserialize Protobuf or JSON to specified structs. The parsed
+// instances could be encoded to a JSON string for display, and `CosmosSDKMsg`s could be used to
+// build a new transaction.
+// FIXME: It seems that these structs are only used for Cosmos parsing for now. They could be
+// moved to `cosmos_sdk.rs` if reusable.
+
+use cosmrs::Any;
+use cosmrs::crypto::{LegacyAminoMultisig, PublicKey};
+use cosmrs::tx::{mode_info, AuthInfo, Fee, ModeInfo, SignerInfo, SignerPublicKey};
 use crate::transaction::cosmos_sdk::{CosmosError, CosmosSDKMsg, SingleCoin};
 use crate::utils::hex_decode;
+use eyre::WrapErr;
+use itertools::Itertools;
 use prost::Message;
 use serde::{Deserialize, Serialize};
 
@@ -18,14 +24,48 @@ pub struct CosmosAny {
     pub value: String,
 }
 
+impl From<Any> for CosmosAny {
+    fn from(any: Any) -> Self {
+        Self {
+            type_url: any.type_url,
+            value: base64::encode(any.value),
+        }
+    }
+}
+
+impl TryFrom<PublicKey> for CosmosAny {
+    type Error = CosmosError;
+
+    fn try_from(public_key: PublicKey) -> Result<Self, Self::Error> {
+        Ok(public_key.to_any()?.into())
+    }
+}
+
 /// AuthInfo describes the fee and signer modes that are used to sign a transaction.
 #[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CosmosAuthInfo {
-    /// Signing modes for the required signers
-    pub signer_infos: Vec<CosmosSignerInfo>,
     /// Fee and gas limit
     pub fee: CosmosFee,
+    /// Signing modes for the required signers
+    pub signer_infos: Vec<CosmosSignerInfo>,
+}
+
+impl TryFrom<AuthInfo> for CosmosAuthInfo {
+    type Error = CosmosError;
+
+    fn try_from(auth_info: AuthInfo) -> Result<Self, Self::Error> {
+        let signer_infos = auth_info
+            .signer_infos
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect::<Result<_, _>>()?;
+
+        Ok(Self {
+            fee: auth_info.fee.into(),
+            signer_infos,
+        })
+    }
 }
 
 /// Body of a transaction that all signers sign over.
@@ -57,6 +97,30 @@ pub struct CosmosFee {
     pub granter: Option<String>,
 }
 
+impl From<Fee> for CosmosFee {
+    fn from(fee: Fee) -> Self {
+        let amount = fee
+            .amount
+            .into_iter()
+            .map(|coin|
+            // FIXME:
+            // It seems unnecessary to convert to definite Enum value of `SingleCoin`. Since it is
+            // only used for display or converting back to `cosmrs::Coin`.
+            SingleCoin::Other {
+                amount: coin.amount.to_string(),
+                denom: coin.denom.to_string(),
+            })
+            .collect();
+
+        Self {
+            amount,
+            gas_limit: fee.gas_limit.value(),
+            payer: fee.payer.map(|p| p.to_string()),
+            granter: fee.granter.map(|g| g.to_string()),
+        }
+    }
+}
+
 /// Legacy Amino multisig key.
 #[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -67,6 +131,21 @@ pub struct CosmosLegacyAminoMultisig {
     pub public_keys: Vec<CosmosAny>,
 }
 
+impl TryFrom<LegacyAminoMultisig> for CosmosLegacyAminoMultisig {
+    type Error = CosmosError;
+
+    fn try_from(multisig: LegacyAminoMultisig) -> Result<Self, CosmosError> {
+        Ok(Self {
+            threshold: multisig.threshold.into(),
+            public_keys: multisig
+                .public_keys
+                .into_iter()
+                .map(TryInto::try_into)
+                .collect::<Result<_, _>>()?,
+        })
+    }
+}
+
 /// ModeInfo describes the signing mode of a single or nested multisig signer.
 #[derive(Deserialize, Serialize)]
 pub enum CosmosModeInfo {
@@ -74,6 +153,19 @@ pub enum CosmosModeInfo {
     Single { mode: String },
     /// Nested multisig signer
     Multi { modes: Vec<String> },
+}
+
+impl From<ModeInfo> for CosmosModeInfo {
+    fn from(mode_info: ModeInfo) -> Self {
+        match mode_info {
+            ModeInfo::Single(_) => Self::Single {
+                mode: format_mode_info(mode_info),
+            },
+            ModeInfo::Multi(mode_info::Multi { mode_infos, .. }) => Self::Multi {
+                modes: mode_infos.into_iter().map(format_mode_info).collect(),
+            },
+        }
+    }
 }
 
 /// SignerInfo describes the public key and signing mode of a single top-level signer.
@@ -88,6 +180,20 @@ pub struct CosmosSignerInfo {
     pub sequence: u64,
 }
 
+impl TryFrom<SignerInfo> for CosmosSignerInfo {
+    type Error = CosmosError;
+
+    fn try_from(signer_info: SignerInfo) -> Result<Self, Self::Error> {
+        let public_key = signer_info.public_key.map(TryInto::try_into).transpose()?;
+
+        Ok(Self {
+            public_key,
+            mode_info: signer_info.mode_info.into(),
+            sequence: signer_info.sequence,
+        })
+    }
+}
+
 /// Signer's public key.
 #[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -100,23 +206,51 @@ pub enum CosmosSignerPublicKey {
     Any { key: CosmosAny },
 }
 
-/// Create a Cosmos parser for `crypto.org`.
+impl TryFrom<SignerPublicKey> for CosmosSignerPublicKey {
+    type Error = CosmosError;
+
+    fn try_from(signer_public_key: SignerPublicKey) -> Result<Self, Self::Error> {
+        Ok(match signer_public_key {
+            SignerPublicKey::Single(public_key) => Self::Single {
+                key: public_key.try_into()?,
+            },
+            SignerPublicKey::LegacyAminoMultisig(multisig) => Self::LegacyAminoMultisig {
+                key: multisig.try_into()?,
+            },
+            SignerPublicKey::Any(any) => Self::Any { key: any.into() },
+        })
+    }
+}
+
+/// Create a Cosmos parser for `crypto.org` chain.
 pub fn new_crypto_org_parser() -> impl CosmosParser {
     CryptoOrgParser {}
 }
 
-// TODO: Add a Cosmos parser for Terra.
+/// Create a Cosmos parser for `Terra` chain.
+pub fn new_terra_parser() -> impl CosmosParser {
+    TerraParser {}
+}
 
 /// Cosmos parser trait
 pub trait CosmosParser {
-    ///
+    /// Parse `CosmosAuthInfo` from hex data of Protobuf.
     fn parse_proto_auto_info(&self, hex_string: &str) -> Result<CosmosAuthInfo, CosmosError> {
-        todo!();
-        // let bytes = hex_decode(hex_string)?;
-        // cosmos_sdk_proto::cosmos::tx::v1beta1::AuthInfo::decode(bytes.as_slice())?.try_into()
+        let bytes = hex_decode(hex_string).wrap_err("Failed to decode hex string")?;
+        let auth_info = AuthInfo::try_from(
+            cosmos_sdk_proto::cosmos::tx::v1beta1::AuthInfo::decode(bytes.as_slice())
+                .wrap_err("Failed to decode AuthInfo from Protobuf")?,
+        )?;
+        auth_info.try_into()
     }
 
+    /// Parse `CosmosBody` from hex data of Protobuf.
     fn parse_proto_body(&self);
+    /*
+    fn parse_amino_msgs(&self);
+    fn parse_amino_fee(&self);
+    fn parse_amino_gas_price(&self);
+    */
 }
 
 struct NormalParser {}
@@ -129,6 +263,22 @@ struct CryptoOrgParser {}
 
 impl CosmosParser for CryptoOrgParser {
     fn parse_proto_body(&self) {}
+}
+
+struct TerraParser {}
+
+impl CosmosParser for TerraParser {
+    fn parse_proto_body(&self) {}
+}
+
+fn format_mode_info(mode_info: ModeInfo) -> String {
+    match mode_info {
+        ModeInfo::Single(mode_info::Single { mode }) => format!("{mode:?}"),
+        ModeInfo::Multi(mode_info::Multi { mode_infos, .. }) => {
+            let modes = mode_infos.into_iter().map(format_mode_info).join(", ");
+            format!("[{modes}]")
+        }
+    }
 }
 
 #[cfg(test)]
