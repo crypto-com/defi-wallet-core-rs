374861e4be6bb6b065e8726a97991caf72359886
Damon Chen <damon.chen@crypto.com>
2022-05-31 13:28:49 +0800

    Add pagination parameter for uniffi

--- bindings/cpp/src/nft.cc
+++ bindings/cpp/src/nft.cc
@@ -2,10 +2,12 @@
 
 namespace org {
 namespace defi_wallet_core {
-    Pagination::Pagination() : enable{false}, offset{0}, limit{100}, count_total{false}, reverse{false} {}
+Pagination::Pagination()
+    : enable{false}, offset{0}, limit{100}, count_total{false}, reverse{false} {
+}
 
 bool Pagination::get_enable() const { return enable; }
-Vec<uint8_t>Pagination::get_key() const { return key; }
+Vec<uint8_t> Pagination::get_key() const { return key; }
 uint64_t Pagination::get_offset() const { return offset; }
 uint64_t Pagination::get_limit() const { return limit; }
 bool Pagination::get_count_total() const { return count_total; }
