c3b581fb65c05f07bd210dba818cdb8c01c2cea7
SHH-HaoZhang <91600099+SHH-HaoZhang@users.noreply.github.com>
2022-08-26 16:50:32 +0800

    Problem: flaky and slow Android integration test (fix #566) (#567)
    Solution:
    * Add system-images cache in android CI
    
    * fix android ci
    
    * rename android system cache

--- android_build.sh
+++ android_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" == "x86" ]; then
+        echo "Build x86 only"
+fi
+
 if [ ! -n "$NDK_HOME" ]; then
         echo "Env NDK_HOME is empty"
         exit 1
@@ -31,21 +35,27 @@ fi
 
 uniffi-bindgen generate common/src/common.udl --config common/uniffi.toml --language kotlin --out-dir bindings/android || exit 1
 
-rustup target add aarch64-linux-android armv7-linux-androideabi x86_64-linux-android || exit 1
+if [ "$1" != "x86" ]; then
+        rustup target add aarch64-linux-android armv7-linux-androideabi || exit 1
+fi
+rustup target add x86_64-linux-android || exit 1
 
 type python || exit 1
-if [ ! -d "NDK/arm64" ]
-then
-        "$MAKETOOL" --api 28 --arch arm64 --install-dir NDK/arm64 2> /dev/null || exit 1
-else
-        echo "arm64 ndk installed."
-fi
 
-if [ ! -d "NDK/arm" ]
-then
-        "$MAKETOOL" --api 28 --arch arm --install-dir NDK/arm 2> /dev/null || exit 1
-else
-        echo "arm ndk installed."
+if [ "$1" != "x86" ]; then
+        if [ ! -d "NDK/arm64" ]
+        then
+                "$MAKETOOL" --api 28 --arch arm64 --install-dir NDK/arm64 2> /dev/null || exit 1
+        else
+                echo "arm64 ndk installed."
+        fi
+
+        if [ ! -d "NDK/arm" ]
+        then
+                "$MAKETOOL" --api 28 --arch arm --install-dir NDK/arm 2> /dev/null || exit 1
+        else
+                echo "arm ndk installed."
+        fi
 fi
 
 if [ ! -d "NDK/x86_64" ]
@@ -55,19 +65,23 @@ else
         echo "x86_64 ndk installed."
 fi
 
-PATH=$PATH:`pwd`/NDK/arm64/bin cargo build --features uniffi-binding --target aarch64-linux-android -p defi-wallet-core-common --release || exit 1
-PATH=$PATH:`pwd`/NDK/arm/bin cargo build --features uniffi-binding --target armv7-linux-androideabi -p defi-wallet-core-common --release || exit 1
+if [ "$1" != "x86" ]; then
+        PATH=$PATH:`pwd`/NDK/arm64/bin cargo build --features uniffi-binding --target aarch64-linux-android -p defi-wallet-core-common --release || exit 1
+        PATH=$PATH:`pwd`/NDK/arm/bin cargo build --features uniffi-binding --target armv7-linux-androideabi -p defi-wallet-core-common --release || exit 1
+fi
 PATH=$PATH:`pwd`/NDK/x86_64/bin cargo build --features uniffi-binding --target x86_64-linux-android -p defi-wallet-core-common --release || exit 1
 
 type strip || exit 1
 mkdir -p mobile_modules/android_module/dwclib/libs
 cp NDK/libs/jna.aar mobile_modules/android_module/dwclib/libs/
-mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a || exit 1
-cp target/aarch64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so || exit 1
-strip mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so
-mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a || exit 1
-cp target/armv7-linux-androideabi/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so || exit 1
-strip mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so
+if [ "$1" != "x86" ]; then
+        mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a || exit 1
+        cp target/aarch64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so || exit 1
+        strip mobile_modules/android_module/dwclib/src/main/jniLibs/arm64-v8a/libdwc-common.so
+        mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a || exit 1
+        cp target/armv7-linux-androideabi/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so || exit 1
+        strip mobile_modules/android_module/dwclib/src/main/jniLibs/armeabi-v7a/libdwc-common.so
+fi
 mkdir -p mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64 || exit 1
 cp target/x86_64-linux-android/release/libdefi_wallet_core_common.so mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so || exit 1
 strip mobile_modules/android_module/dwclib/src/main/jniLibs/x86_64/libdwc-common.so
