13f85aa5d4aeb92f2e78b2d5b5745bc43046cf07
SHH-HaoZhang <hao.zhang@crypto.com>
2022-08-31 13:39:11 +0800

    remove we_alloc

--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -12,12 +12,6 @@ pub use cosmos_sdk::*;
 pub use ethereum::*;
 pub use utils::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 /// HD Wallet wrapper for Wasm
 #[wasm_bindgen]
 pub struct Wallet {
