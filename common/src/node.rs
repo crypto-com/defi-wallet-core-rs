e92916d19f47c41d3ef1373088d08d6418b9d98b
jonghwan <jonghwan@crypto.com>
2023-06-08 19:21:18 +0900

    Problem: cargo check fails (fix #921)
    skip check

--- common/src/node.rs
+++ common/src/node.rs
@@ -1,3 +1,5 @@
+#![allow(ambiguous_glob_reexports)]
+
 /// wrappers around Cosmos SDK REST API and Tendermint RPC
 /// FIXME: switch to grpc when grpc-web works in CosmRS: https://github.com/cosmos/cosmos-rust/pull/157
 mod cosmos_sdk;
